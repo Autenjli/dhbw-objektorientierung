d4fe2c6d3f78e33e332d2781604fb1c23990b7e1
autenrieth.julian <autenrieth.julian@348A18>
2019-11-14 14:27:03 +0100

    Neuer Code

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -1,7 +1,4 @@
-
-
 #include "stdafx.h"
-
 #include <Gosu/Gosu.hpp>
 #include <Gosu/AutoLink.hpp>
 #include <cstdint>
@@ -15,30 +12,20 @@
 using namespace std;
 
 
-// Simulationsgeschwindigkeit
-const double DT = 100.0;
-
-
-const int PixelX = 800;
+const double DT = 100.0; // Simulationsgeschwindigkeit
+const int PixelX = 800;//Fensterabmessungen
 const int PixelY = 600;
-const int SledLen = 50;
-const double StartVel = 7;
+const int SledLen = 50;//Schlittenlänge einer Schlitteneinheit
+const double StartVel = 7;//Grund bzw Startgeschwindigkeit des Balls
 
 
+class GameWindow : public Gosu::Window {
 
-class GameWindow : public Gosu::Window
-{
 public:
-	Gosu::Image bild;
-	GameWindow()
-		: Window(PixelX, PixelY)
-		, bild("rakete.png")
+	GameWindow() : Window(PixelX, PixelY)
 	{
-		//set_caption("Gosu Tutorial Game mit Git");
+		set_caption("Schaeffler Shooter");
 	}
-
-
-
 
 
 	class BALL {
@@ -53,25 +40,20 @@ public:
 		void set_vx(double x) {
 			this->vx = x;
 		}
-
 		void set_vy(double y) {
 			this->vy = y;
 		}
-
 		void set_amout(double a) {
 			this->amout = StartVel * a;
 			this->vy = sqrt(this->amout)*this->vy;
 			this->vx = sqrt(this->amout)*this->vx;
 		}
-
 		double get_vx() {
 			return this->vx;
 		}
-
 		double get_vy() {
 			return this->vy;
 		}
-
 		double get_amout() {
 			return this->amout;
 		}
@@ -81,7 +63,6 @@ public:
 		int len = 2;
 		double x;
 	public:
-		boolean skill;
 
 		void set_len(int x) {
 			this->len = x;
@@ -118,40 +99,37 @@ public:
 		double wx;
 		double wy;
 		int hit;
-		Gosu::Color c;
 
 	public:
-		BLOCK(double X, double Y, double wX, double wY, int h, Gosu::Color C) {
+		BLOCK(double X, double Y, double wX, double wY, int h) {
 			this->x = X;
 			this->y = Y;
 			this->wx = wX;
 			this->wy = wY;
 			this->hit = h;
-			this->c = C;
 		}
 
 		double get_x() {
 			return this->x;
 		}
-
 		double get_y() {
 			return this->y;
 		}
-
+		double get_y2() {
+			return this->y + this->wy;
+		}
+		double get_x2() {
+			return this->x + this->wx;
+		}
 		double get_wx() {
 			return this->wx;
 		}
-
 		double get_wy() {
 			return this->wy;
 		}
 		int get_hit() {
 			return this->hit;
 		}
-		Gosu::Color get_color() {
-			return this->c;
-		}
-
 		void red_hit() {
 			if (this->hit >= 1) {
 				this->hit = this->hit - 1;
@@ -159,154 +137,166 @@ public:
 			else {
 				this->hit = 0;
 			}
-
 		}
 	};
 
-	class LEVEL {
-	public:
-		int number;
-		list<BLOCK> block;
-	};
+
+
 
 	BALL ball;
 	SLED sled;
-	double A;
-	double B;
-	double C;
-	double D;
-	double LineX;
-	double LineY;
-	int LevelCount = 1;
-	LEVEL L1;
-	enum stat { Init, Start, Play, NextLevel };
+	list<BLOCK> level;
+
+	int LevelCount = 0, life = 10;
+	double StartLineX, StartLineY, C;
+	list<BLOCK>::iterator iter, iter_erase;
+	bool hit = false;
+
+	enum stat { Init, Start, Play };
 	stat status = Init;
-	list<BLOCK>::iterator iter;
+
+	Gosu::Color ColorList[10] = { Gosu::Color::WHITE,Gosu::Color::RED,Gosu::Color::YELLOW,Gosu::Color::GREEN,Gosu::Color::CYAN,Gosu::Color::BLUE,Gosu::Color::FUCHSIA,Gosu::Color::GRAY };
+
+
+	void LevelSetup(int nr) {//Aufbau der Level
+
+							 //Aufbau:
+							 //level.push_front(BLOCK(x-pos,y-pos,breite,höhe,Anzahl_Treffer));
+
+		switch (nr) {
+
+		case 0:
+			level.push_front(BLOCK(300, 200, 10, 10, 1));
+			level.push_front(BLOCK(100, 50, 50, 50, 2));
+			level.push_front(BLOCK(150, 50, 50, 50, 3));
+			level.push_front(BLOCK(200, 50, 50, 50, 4));
+			level.push_front(BLOCK(250, 50, 50, 50, 5));
+
+			break;
+
+		case 1:
+			level.push_front(BLOCK(300, 200, 200, 200, 1));
+			break;
+
+		case 2:
+			level.push_front(BLOCK(150, 50, 50, 50, 3));
+			break;
+
+		}
+	}
 
 
 
 
-	// wird bis zu 60x pro Sekunde aufgerufen.
-	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
-	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	void draw() override
-	{
 
-		//graphics().draw_line(10, 20, Gosu::Color::RED,200, 100, Gosu::Color::GREEN,0.0);
-		//graphics().draw_triangle(x, y, Gosu::Color::RED, x+200, y+200, Gosu::Color::GREEN, x+100, y+20, Gosu::Color::BLUE, 0.0);
-		//bild.draw_rot(50, 50,0.0, x, 0.5);
-		graphics().draw_rect(A, PixelY - 5, B, 5, Gosu::Color::GREEN, 0.0);
-		graphics().draw_rect(ball.x - 5, ball.y - 5, 10, 10, Gosu::Color::GREEN, 0.0);
 
-		for (auto list : L1.block) {
-			if (list.get_hit() > 0) {
-				graphics().draw_rect(list.get_x(), list.get_y(), list.get_wx(), list.get_wy(), list.get_color(), 0.0);
-			}
+	void draw() override {
+
+		//Noch in Arbeit...
+		//Gosu::Font f(5, "" , 1); 
+		//f.draw_rel(text, PixelX-50, 20, 1, 1, 1, 3, 3, Gosu::Color::WHITE);
+
+		graphics().draw_rect(sled.get_x(), PixelY - 5, sled.get_len()* SledLen, 5, Gosu::Color::GREEN, 0.0);//Schlitten zeichnen
+		graphics().draw_rect(ball.x - 5, ball.y - 5, 10, 10, Gosu::Color::FUCHSIA, 0.0);//Ball zeichnen
+
+		for (auto i : level) {
+			graphics().draw_rect(i.get_x(), i.get_y(), i.get_wx(), i.get_wy(), ColorList[i.get_hit()], 0.0);//Blöcke aus Blockliste zeichenen		
 		}
 
 		if (status == Start) {
-			graphics().draw_line(PixelX / 2, PixelY - 10, Gosu::Color::GREEN, LineX, LineY, Gosu::Color::GREEN, 0.0);
+			graphics().draw_line(PixelX / 2, PixelY - 10, Gosu::Color::GREEN, StartLineX, StartLineY, Gosu::Color::GREEN, 0.0);//Linie für Start zeichnen
 		}
 	}
 
-	// Wird 60x pro Sekunde aufgerufen
-	void update() override
-	{
+	void update() override {
 
-		switch (status) {
+		switch (status) {//Spiellogik je nach Status ausführen
 
-		case Init:
-
-			L1.block.push_front(BLOCK(300, 200, 200, 200, 5, Gosu::Color::RED));
-			L1.block.push_front(BLOCK(100, 50, 45, 45, 2, Gosu::Color::YELLOW));
-			L1.block.push_front(BLOCK(150, 50, 45, 45, 2, Gosu::Color::BLUE));
-			L1.block.push_front(BLOCK(200, 50, 45, 45, 2, Gosu::Color::GRAY));
-			L1.block.push_front(BLOCK(250, 50, 45, 45, 2, Gosu::Color::AQUA));
+		case Init: {
+			LevelSetup(LevelCount);//Blöcke je nach Level in Liste laden
 			status = Start;
 			break;
+		}
 
-
-		case Start:
-			ball.x = PixelX / 2;
+		case Start: {
+			ball.x = PixelX / 2;//Ball auf Grundposition
 			ball.y = PixelY - 10;
-			C = C + 0.1;
-			LineX = PixelX / 2 + cos(C) * 50;
-			LineY = PixelY - 10 - abs(sin(C)) * 50;
+
+			C = C + 0.1;//Laufvariable für Kreisbewegung
+			StartLineX = PixelX / 2 + cos(C) * 50;//Berechnung Punkt für Linie
+			StartLineY = PixelY - 10 - abs(sin(C)) * 50;
 
 			if (input().down(Gosu::MS_LEFT)) {
-				ball.set_amout(1);
-				ball.set_vx((cos(C) + 0.1)*ball.get_amout());
-				ball.set_vy(-abs((sin(C)) + 0.1)*ball.get_amout());
-
+				ball.set_amout(1);//Geschwindigkeit auf einmal StartVel setzen
+				ball.set_vx((cos(C))*ball.get_amout() + 2);//vy je nach Winkel setzen
+				ball.set_vy(-1 * sqrt(ball.get_amout()*ball.get_amout() - ball.get_vx()*ball.get_vx()));//vy nach vx berechnen
 				status = Play;
 			}
-
 			break;
+		}
 
-		case Play:
-			ball.x = ball.x + ball.get_vx();
+		case Play: {
+			ball.x = ball.x + ball.get_vx();//Ball auf neue Position
 			ball.y = ball.y + ball.get_vy();
 
 
-			for (iter = L1.block.begin(); iter != L1.block.end(); iter++) {
+			for (iter = level.begin(); iter != level.end(); iter++) {//Schleife über Blöcke
 
+				if (ball.y >= iter->get_y() && ball.y <= iter->get_y2() && ball.x >= iter->get_x() && ball.x <= iter->get_x2()) {//Prüfung auf Treffer
+					iter->red_hit();//Hit-Couter reduzieren
 
-				/*
-
-				if (ball.y >= *iter.get_y() && ball.y <= list.get_y() + list.get_wy() && ball.x >= list.get_x() && ball.x <= list.get_x() + list.get_wx()) {
-
-
-
-				if (ball.x > list.get_x() + abs(ball.get_vx()) && ball.x < list.get_x() + list.get_wx() - abs(ball.get_vx())) {
-				ball.set_vy(ball.get_vy()*-1);
+					if (iter->get_hit() <= 0) {//Merker um Block später zu löschen
+						hit = true;
+						iter_erase = iter;
+					}
+					if (ball.x > iter->get_x() + abs(ball.get_vx()) && ball.x < iter->get_x2() - abs(ball.get_vx())) {//Prüfen wo Ball am Block aufkommt und entsprechend abprallen lassen
+						ball.set_vy(ball.get_vy()*-1);
+					}
+					else {
+						ball.set_vx(ball.get_vx() *-1);
+					}
 				}
-				else {
-				ball.set_vx(ball.get_vx() *-1);
-				}
-
-
-
-				//if (list.get_hit() <= 0) {
-				//L1.block.erase(list);
-				}
-
-				*/
 			}
-		}
 
+			if (hit) {//Block löschen falls Treffer vorhanden
+				level.erase(iter_erase);
+				hit = false;
+			}
 
-		if (ball.y >= PixelY) {
-			status = Start;
-		}
-		break;
+			if (ball.y >= PixelY) {//Leben reduzieren und zurück zum Start falls Ball verloren
+				status = Start;
+				life = life - 1;
+				if (life <= 0) {//Spiel rücksetzen falls keine Leben mehr
+					life = 10;
+					LevelCount = 0;
+				}
+			}
 
-		case NextLevel:
+			if (level.size() == 0) {//nächstes Level falls keine Blöcke mehr
+				LevelCount++;
+				status = Start;
+			}
 			break;
-	}
+		}
+		}
 
+		sled.set_x(input().mouse_x());//Mausposition für Schlitten abfragen
 
-	sled.set_x(input().mouse_x());
-	A = sled.get_x();
-	B = sled.get_len()* SledLen;
-
-
-	if (ball.x <= 0 || ball.x >= PixelX) {
-		ball.set_vx(ball.get_vx()*-1);
+		if (ball.x <= 0 || ball.x >= PixelX) {//Ball rechts und links abprallen
+			ball.set_vx(ball.get_vx()*-1);
+		}
+		if (ball.y <= 0) {//Ball oben abprallen
+			ball.set_vy(ball.get_vy() *-1);
+		}
+		if (ball.y >= PixelY - 5 && ball.x >= sled.get_x() && ball.x <= sled.get_x() + sled.get_len()*SledLen && ball.get_vy() > 0) {//Ball am Schlitten abprallen lassen
+			ball.set_vx((1.8*(ball.x - sled.get_x()) / (sled.get_len()*SledLen) - 0.9)*ball.get_amout());//Betragsfunktion für vx um Abprallwinkel zu erzwugen
+			ball.set_vy(sqrt(pow(ball.get_amout(), 2) - pow(ball.get_vx(), 2))*-1);//vy nach vx anpassen
+		}
 	}
-	if (ball.y <= 0) {
-		ball.set_vy(ball.get_vy() *-1);
-	}
-	if (ball.y >= PixelY - 5 && ball.x >= sled.get_x() && ball.x <= sled.get_x() + sled.get_len()*SledLen && ball.get_vy()>0) {
-		ball.set_vx((1.8*(ball.x - sled.get_x()) / (sled.get_len()*SledLen) - 0.9)*ball.get_amout());
-		ball.set_vy(sqrt(pow(ball.get_amout(), 2) - pow(ball.get_vx(), 2))*-1);
-	}
-}
 };
 
 // C++ Hauptprogramm
-int main()
-{
+int main() {
 	GameWindow window;
 	window.show();
 }
-
